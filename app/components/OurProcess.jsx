74c60683794619d9f0edbf4cff7522abacfa2def
UnaizaZafar <unaizaofficial840@gmail.com>
2025-11-04 20:43:53 +0500

    added responsive to process section

--- app/components/OurProcess.jsx
+++ app/components/OurProcess.jsx
@@ -3,11 +3,11 @@ import ProcessCard from "./ProcessCard"
 
 const OurProcess = () => {
     return (
-        <div className="py-37.5 flex flex-col gap-4 items-center max-w-225 w-full mx-auto">
-            <div className="bg-white text-neutral-900 border border-dashed border-grey rounded-4xl pl-4 pr-2.5 py-1.5 flex gap-1.5 items-center">{star} Our Process</div>
+        <div className="py-20 md:py-30 lg:py-37.5 flex flex-col gap-3 md:gap-4 items-center max-w-11/12 md:max-w-225 w-full md:mx-auto max-sm:px-4 max-md:px-6">
+            <div className="bg-white text-neutral-900 border border-dashed border-grey rounded-4xl pl-4 pr-2.5 py-1.5 flex gap-1.5 items-center text-sm sm:text-base">{star} Our Process</div>
             <div className="flex flex-col gap-0.5 items-center">
-                <p className="font-semibold text-5xl text-neutral-900 leading-14 text-center tracking-tightest">How our Approach</p>
-                <p className="font-medium text-lg leading-7 tracking-0.18 max-w-116.5 text-center">A quick overview of how we work together to make your </p>
+                <p className="font-semibold text-3xl sm:text-4xl md:text-5xl text-neutral-900 leading-tight md:leading-14 text-center tracking-tightest">How our Approach</p>
+                <p className="font-medium text-sm sm:text-base md:text-lg leading-relaxed md:leading-7 tracking-0.18 max-w-11/12 md:max-w-116.5 w-full text-center">A quick overview of how we work together to make your </p>
             </div>
                 <ProcessCard />
             {/* </div> */}
--- app/components/ProcessCard.jsx
+++ app/components/ProcessCard.jsx
@@ -2,21 +2,21 @@ import { ourApproachCardsData } from "@/public/utils/data"
 import Image from "next/image"
 const ProcessCard = () => {
     return (
-        <div className="pt-8.5 grid grid-cols-2 gap-2.5">
+        <div className="pt-8.5 grid grid-cols-1 md:grid-cols-2 gap-2.5">
             {
                 ourApproachCardsData.map((item) => (
-                    <div className={` rounded-20 border border-grey border-dashed bg-white drop-shadow-custom pt-2.5 pl-2.5 w-full overflow-hidden flex flex-col justify-between ${item.id === ourApproachCardsData.length && "col-span-2"}`} key={item.id}>
-                        <div className="flex overflow-hidden h-max">
-                            <div className="border border-grey border-dashed px-3.5 py-1 rounded-xl text-lg font-medium text-neutral-900 leading-6 tracking-[-1px]
+                    <div className={` rounded-20 border border-grey border-dashed bg-white drop-shadow-custom w-full overflow-hidden flex flex-col justify-between ${item.id === ourApproachCardsData.length && "md:col-span-2"}`} key={item.id}>
+                        <div className={`flex  ${item.id === ourApproachCardsData.length ? "justify-center" : "justify-end"} overflow-hidden h-max relative`}>
+                            <div className="absolute left-2.5 top-2.5 border border-grey border-dashed px-3.5 py-1 rounded-xl text-base md:text-lg font-medium text-neutral-900 leading-6 tracking-[-1px]
                              bg-snow-white h-max">{item.number}</div>
-                            <div className={`${item.id === ourApproachCardsData.length && "pt-7"}`}>
+                            <div className={`pt-7`}>
                                 <Image src={`/icons/${item.svg}.svg`} alt="icon" width={0} height={0} unoptimized className="w-full h-full" />
                             </div>
                         </div>
                         {/* text field */}
                         <div className="px-4 pb-6.5 pt-3.5 flex flex-col gap-2.5 bg-white">
-                            <p className="font-medium text-xl leading-7 text-neutral-900">{item.title}</p>
-                            <p className="text-gray-900 tracking-[-0.018em] leading-6">{item.desc}</p>
+                            <p className="font-medium text-lg sm:text-xl leading-6 sm:leading-7  text-neutral-900">{item.title}</p>
+                            <p className="text-gray-900 text-sm sm:text-base tracking-[-0.018em] leading-6">{item.desc}</p>
                         </div>
                     </div>
                 ))
